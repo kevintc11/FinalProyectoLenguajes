262d43b17b70c382cf4685e8ee082296c8749f79
Juan Carlos Valverde Araya <juan.valverde@ucrso.info>
2020-06-24 11:33:21 -0600

    MenuAdmin

---
+++ FinalProyectoLenguajes/Interfaz/MenuAdmin.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MenuAdmin.aspx.cs" Inherits="Interfaz.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style="text-align: center">
+    <form id="form1" runat="server">
+        <div>
+            Menú Administrador<br />
+            Bienvenido
+            <asp:Label ID="lbName" runat="server" Text="*Nombre*"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btDishes" runat="server" Height="35px" Text="Administrar Platos" Width="140px" />
+&nbsp;&nbsp;
+            <asp:Button ID="btUsers" runat="server" Height="35px" Text="Administrar Usuarios" Width="140px" />
+            <br />
+            <br />
+            <asp:Button ID="btClient" runat="server" Height="35px" Text="Bloquear Clientes" Width="140px" />
+&nbsp;&nbsp;
+            <asp:Button ID="btPed" runat="server" Height="35px" Text="Administrar Pedidos" Width="140px" />
+            <br />
+            <br />
+            <asp:Button ID="btBack" runat="server" Text="Salir" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ FinalProyectoLenguajes/Interfaz/MenuAdmin.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Interfaz
+{
+
+
+    public partial class WebForm2
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lbName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbName;
+
+        /// <summary>
+        /// Control btDishes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btDishes;
+
+        /// <summary>
+        /// Control btUsers.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btUsers;
+
+        /// <summary>
+        /// Control btClient.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btClient;
+
+        /// <summary>
+        /// Control btPed.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btPed;
+
+        /// <summary>
+        /// Control btBack.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btBack;
+    }
+}
